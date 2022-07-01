ea915f305d865e020920e0f5fa1eec5666eae979
yayxs <yangwanli@wanji.net.cn>
2022-07-02 07:25:21 +0800

    f6 datav

---
+++ pages/index/data.js
@@ -0,0 +1,40 @@
+export default {
+  id: 'ZM互动',
+  children: [
+    {
+      id: '万境蓝山梧桐苑',
+      children: [
+        { id: '临时供电' },
+     
+      ],
+    },
+    {
+      id: '龙湖江与城',
+      children: [
+        {
+          id: '21栋01户型外墙出现排烟管道',
+         
+        },
+        
+      ],
+    },
+    {
+          id: '岳麓区时代梅溪领峰小区',
+          children: [{ id: '物业以及住建问题的投诉' }],
+        },
+        {
+          id: '荣盛岳麓峰景小区',
+          children: [{ id: '物业摆烂' }, ],
+        },
+    {
+      id: 'Regression',
+      children: [
+        { id: 'Multiple linear regression' },
+        { id: 'Partial least squares' },
+        { id: 'Multi-layer feedforward neural network' },
+        { id: 'General regression neural network' },
+        { id: 'Support vector regression' },
+      ],
+    },
+  ],
+};
